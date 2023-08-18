af08f2e99f1a134a0af39b6cf0ff1fddfecdd0f7
Antonina <tonaserbenko@gmail.com>
2023-08-18 16:23:46 +0300

    Initial commit

---
+++ assets/css/index.js
@@ -0,0 +1,61 @@
+let a = 5;
+let b = 3;
+let sum = a*b;
+console.log (sum);
+
+let c = 10;
+let d = 5;
+sum = c/d;
+console.log (sum); 
+
+let e = 50;
+let f = 25;
+sum = e+f;
+console.log(sum);
+
+console.log(11);
+
+console.log(true);
+
+console.log('"java script"');
+
+console.log('"100"');
+
+let result = prompt("What number?");
+sum = result**2;
+alert(sum);
+console.log(sum);
+
+let number1 = prompt("First number?");
+let number2 = prompt("Second number?");
+sum = (+number1+ +number2)/2;
+console.log(sum);
+
+let minutes = prompt("How many minutes?");
+sum = 60*minutes;
+alert(sum);
+console.log(sum);
+
+ let greeting = ("Hello,");
+ let userName = prompt("What is your name?");
+ alert(greeting+userName);
+ 
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
